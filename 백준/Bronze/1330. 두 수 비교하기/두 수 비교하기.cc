4e911d68a28676372d4b05e5a1ce237b7cb03768
Kimtaeyi <111646357+Adsenser@users.noreply.github.com>
2022-10-01 23:08:27 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int num1;
+    int num2;
+
+    std::cin >> num1 >> num2 ;
+
+    if ( num1 > num2)
+    {
+        std::cout << ">" <<std::endl;
+    }
+    else if ( num1 < num2)
+    {
+        std::cout << "<" <<std::endl;
+    }
+    else
+    {
+        std::cout << "==" << std::endl;
+    }
+    return 0;
+}
